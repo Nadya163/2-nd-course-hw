743590d03a61416b0116401a163f6b24ba1e1929
Nadya163 <satara19892202@gmail.com>
2023-05-13 00:07:24 +0600

    Задание 5

--- script.js
+++ script.js
@@ -13,9 +13,14 @@ alert(firstIPhone);*/
 alert(creatorJS);*/
 
 //Задание 4
-let num1 = 10;
+/*let num1 = 10;
 let num2 = 2;
 alert(num1 + num2);
 alert(num1 - num2);
 alert(num1 * num2);
-alert(num1 / num2);
+alert(num1 / num2);*/
+
+//Задание 5
+let result = 2;
+result = 2 ** 5;
+alert(result);
