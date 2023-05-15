37bb2915736a2531cd770547544c0c0827770b35
Nadya163 <satara19892202@gmail.com>
2023-05-15 23:42:38 +0600

    Задание 2

--- script2.js
+++ script2.js
@@ -1,9 +1,14 @@
 // Заание 1
-let namePassword = prompt('Введите пароль');
-let password = 'пароль';
+// let namePassword = prompt('Введите пароль');
+// let password = 'пароль';
 
-if (namePassword === password) {
-    console.log('Пароль введен верно');
-} else {
-    console.log('Пароль введен не правильно');
-}
+// if (namePassword === password) {
+//     console.log('Пароль введен верно');
+// } else {
+//     console.log('Пароль введен не правильно');
+// }
+
+//Задание 2
+let c = 15;
+
+(c > 0 && c < 10) ? console.log('Верно') : console.log('Не верно');
