4bd00962aacaf129c01db891d8b23d82eae4971f
Nadya163 <satara19892202@gmail.com>
2023-05-15 21:48:03 +0600

    Задание 1

---
+++ script2.js
@@ -0,0 +1,9 @@
+// Заание 1
+let namePassword = prompt('Введите пароль');
+let password = 'пароль';
+
+if (namePassword === password) {
+    console.log('Пароль введен верно');
+} else {
+    console.log('Пароль введен не правильно');
+}
