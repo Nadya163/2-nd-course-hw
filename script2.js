39b1ec5422b89cdfb49cd181d3d95b7e09eef812
Nadya163 <satara19892202@gmail.com>
2023-05-16 21:19:38 +0600

    Задание 5

--- script2.js
+++ script2.js
@@ -25,8 +25,37 @@
 // (d > 100 || e > 100) ? console.log('Верно') : console.log("Не верно");
 
 //Задание 4
-let a = '2';
-let b = '3';
+// let a = '2';
+// let b = '3';
 
-alert(+a + +b);
-//alert( Number(a) + Number(b) );
+// alert(+a + +b);
+// //alert( Number(a) + Number(b) );
+
+//Задание 5
+let monthNumder = Number(prompt('Введите месяц'));
+
+switch (monthNumder) {
+    case 1:
+    case 2:
+    case 12:
+        console.log('Зима');
+        break;
+    case 3:
+    case 4:
+    case 5:
+        console.log('Весна');
+        break;
+    case 6:
+    case 7:
+    case 8:
+        console.log('Лето');
+        break;
+    case 9:
+    case 10:
+    case 11:
+        console.log('Осень');
+        break;
+    default:
+        console.log('Нет такого месяца!');
+        break;
+}
