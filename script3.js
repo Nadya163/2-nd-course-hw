2113b777d9e0b2be147d69baab584a2c42d5216c
Nadya163 <satara19892202@gmail.com>
2023-05-19 02:36:49 +0600

    Задание 2

--- script3.js
+++ script3.js
@@ -1,5 +1,12 @@
 //Задание 1
- for (let i = 0;  i < 2; i++) {
-    console.log('Привет');
- }
+//  for (let i = 0;  i < 2; i++) {
+//     console.log('Привет');
+//  }
 
+//Задание 2
+let i = 1;
+
+while (i <= 5) {
+    console.log(i);
+    i++;
+}
