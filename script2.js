b5b4bfd905b4be525008b7e41473891aacd035cf
Nadya163 <satara19892202@gmail.com>
2023-05-18 21:27:34 +0600

    Задание 9

--- script2.js
+++ script2.js
@@ -74,12 +74,36 @@
 // }
 
 //Задание 8
-let clientOS = Number(prompt('Введите "0" если у вас iOS или "1" - Android'));
+// let clientOS = Number(prompt('Введите "0" если у вас iOS или "1" - Android'));
 
-if (clientOS === 0) {
-    alert('Установите версию приложения для iOS по ссылке');
-} else if (clientOS === 1) {
-    alert('Установите версию приложения для Android по ссылке');
-} else {
-    alert('Не верно ввели данные');
+// if (clientOS === 0) {
+//     alert('Установите версию приложения для iOS по ссылке');
+// } else if (clientOS === 1) {
+//     alert('Установите версию приложения для Android по ссылке');
+// } else {
+//     alert('Не верно ввели данные');
+// }
+
+//Задание 9
+let clientOS = Number(prompt('Введите "0" если у вас iOS или "1" - Android'));
+let clientDeviceYear = Number(prompt('Введите год выпуска вашего устройства в таком формате "ГГГГ"'));
+
+switch (clientOS) {
+    case 0:
+        if (clientDeviceYear >= 2015) {
+            alert('Установите версию приложения для iOS по ссылке');
+        } else {
+            alert('Установите облегченную версию приложения для iOS по ссылке');
+        }
+        break;
+        case 1:
+        if (clientDeviceYear < 2015) {
+            alert('Установите версию приложения для Android по ссылке');
+        } else {
+            alert('Установите облегченную версию приложения для Android по ссылке');
+        }
+        break;
+    default:
+        alert('Не верно ввели данные');
+        break;
 }
