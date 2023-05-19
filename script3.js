81049057bd46af9d615231178ccfefe919a9701f
Nadya163 <satara19892202@gmail.com>
2023-05-19 11:46:25 +0600

    Задание 4

--- script3.js
+++ script3.js
@@ -12,9 +12,23 @@
 // }
 
 //Задание 3
-let i = 7;
+// let i = 7;
 
-while (i <= 22) {
-    console.log(i);
-    i++;
+// while (i <= 22) {
+//     console.log(i);
+//     i++;
+// }
+
+//Задание 4
+let obj = {
+    "Коля": 200,
+    "Вася": 300,
+    "Петя": 400
+}
+
+for (let key in obj) {
+	if (obj[key]) {
+        let salary = obj[key];
+		console.log(`${key} - зарплата ${salary} долларов`);
+    }
 }
