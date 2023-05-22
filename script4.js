75d9aa52e0987fcf1ba4efe6ab7f87a37dc65166
Nadya163 <satara19892202@gmail.com>
2023-05-23 02:04:47 +0600

    Задание 5

--- script4.js
+++ script4.js
@@ -3,8 +3,8 @@
 //     return a < b ? a : b;
 // }
 
-// minValue(7, 12);
-// minValue(5, 5);
+// console.log(minValue(7, 12));
+// console.log(minValue(5, 5));
 
 //Задание 2
 // function numValue() {
@@ -16,33 +16,44 @@
 //     }
 // }
 
-// numValue();
+// console.log(numValue());
 
 //Задание 3.1
 // function number(num) {
 //     console.log(num * num);
 //   }
    
-//   number(15);
+//  console.log(number(15));
 
-//Задание 3.2
+// //Задание 3.2
 // function number(num) {
 //     return num ** 2;
 //   }
    
-//   number(25);
+//  console.log(number(25));
 
 //Задание 4
-let age = Number(prompt('Введите свой возраст'));
+// let age = Number(prompt('Введите свой возраст'));
 
-function checkAge(age) {
-    if (age <= 12 && age >= 0) {
-        return 'Привет, друг!';
-    } else if (age >= 13 && age >= 0) {
-        return 'Добро пожаловать!';
+// function checkAge(age) {
+//     if (age <= 12 && age >= 0) {
+//         return 'Привет, друг!';
+//     } else if (age >= 13 && age >= 0) {
+//         return 'Добро пожаловать!';
+//     } else {
+//         return 'Вы ввели неправильное значение';
+//     }
+// }
+
+// alert(checkAge(age));
+
+//Задание 5
+function num(a, b) {
+    if (isNaN(a) && isNaN(b)) {
+        return 'Одно или оба значения не являются числом';
     } else {
-        return 'Вы ввели неправильное значение';
+        return a * b;
     }
 }
 
-alert(checkAge(age));
+console.log(num(13, 5));
