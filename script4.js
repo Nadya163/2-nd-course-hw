276ddbd4db9a7312665816cd55c33a819ca5ec49
Nadya163 <satara19892202@gmail.com>
2023-05-22 22:02:10 +0600

    Задание 2

--- script4.js
+++ script4.js
@@ -1,7 +1,19 @@
 //Задание 1
-function minValue(a, b) {
-    return a < b ? a : b;
+// function minValue(a, b) {
+//     return a < b ? a : b;
+// }
+
+// minValue(7, 12);
+// minValue(5, 5);
+
+//Задание 2
+function numValue() {
+    let num = prompt('Пожалуйста, введите любое число');
+    if (num % 2 === 0) {
+        return('Число четное');
+    } else {
+        return('Число не четное');
+    }
 }
 
-minValue(7, 12);
-minValue(5, 5);
+numValue();
