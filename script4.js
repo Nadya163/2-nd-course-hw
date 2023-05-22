26822ff6bb2ad90a2d8cf28e1bab5f301673d27f
Nadya163 <satara19892202@gmail.com>
2023-05-22 22:31:14 +0600

    Задание 3.1

--- script4.js
+++ script4.js
@@ -7,13 +7,20 @@
 // minValue(5, 5);
 
 //Задание 2
-function numValue() {
-    let num = prompt('Пожалуйста, введите любое число');
-    if (num % 2 === 0) {
-        return('Число четное');
-    } else {
-        return('Число не четное');
-    }
-}
+// function numValue() {
+//     let num = prompt('Пожалуйста, введите любое число');
+//     if (num % 2 === 0) {
+//         return('Число четное');
+//     } else {
+//         return('Число не четное');
+//     }
+// }
 
-numValue();
+// numValue();
+
+//Задание 3.1
+function number(num) {
+    console.log(num * num);
+  }
+   
+  number(15);
