97b5142fc548d6a0847f5830e8ebe72ecaa2a2ae
Nadya163 <satara19892202@gmail.com>
2023-05-25 13:20:13 +0600

    Задание 8

---
+++ scriptGame.js
@@ -0,0 +1,15 @@
+function playGame() {
+let month = Number(prompt('Введите номер месяца'));
+
+    if (month === 12 || (month === 1 && month === 2)) {
+        console.log('Этот месяц относится к зиме');
+    } else  if (month >= 3 && month <= 5) {
+      console.log('Этот месяц относится к весна');
+    } else  if (month >= 6 && month <= 8) {
+      console.log('Этот месяц относится к лето');
+    } else  if (month >= 9 && month <= 11) {
+      console.log('Этот месяц относится к осень'); 
+     } else {
+      console.log('Таких данных нет');
+    }
+  };
