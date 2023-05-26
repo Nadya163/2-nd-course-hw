675752b8718b69b9e372b146793b19d322a553bd
Nadya163 <satara19892202@gmail.com>
2023-05-26 21:16:04 +0600

    Задание 2

--- script5.js
+++ script5.js
@@ -1,7 +1,11 @@
 // Задача 1
-const num = [1, 5, 4, 10, 0, 3];
+// const num = [1, 5, 4, 10, 0, 3];
 
-for (let i = 0; i < num.length; i++) {
-    if (num[i] === 10) break;
-    console.log(num[i]); 
-}
+// for (let i = 0; i < num.length; i++) {
+//     if (num[i] === 10) break;
+//     console.log(num[i]); 
+// }
+
+// Задача 2
+const num = [1, 5, 4, 10, 0, 3];
+num.indexOf(4);
