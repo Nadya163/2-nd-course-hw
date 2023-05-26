94cd17d1dd85945b50c000e8c9b208add9edeaf2
Nadya163 <satara19892202@gmail.com>
2023-05-26 21:26:39 +0600

    Задача 3

--- script5.js
+++ script5.js
@@ -7,5 +7,9 @@
 // }
 
 // Задача 2
-const num = [1, 5, 4, 10, 0, 3];
-num.indexOf(4);
+// const num = [1, 5, 4, 10, 0, 3];
+// num.indexOf(4);
+
+// Задача 3
+let arr = [1, 3, 5, 10, 20];
+arr = arr.join(' ');
