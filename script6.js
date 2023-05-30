5d5d6e0cf355ef064c990c4facbd24f508d20998
Nadya163 <satara19892202@gmail.com>
2023-05-30 23:42:48 +0600

    адание 3

--- script6.js
+++ script6.js
@@ -2,14 +2,23 @@
 // console.log('Hello word'.toUpperCase());
 
 // Задание 2
-function strAndArr(arr, str) {
-    let newArr = [];
-    arr.forEach((el) => {
-        if (el.toLowerCase().startsWith(str.toLowerCase())) {
-            newArr.push(el);
-        }
-    });
-    console.log(newArr);
-}
+// function strAndArr(arr, str) {
+//     let newArr = [];
+//     arr.forEach((el) => {
+//         if (el.toLowerCase().startsWith(str.toLowerCase())) {
+//             newArr.push(el);
+//         }
+//     });
+//     console.log(newArr);
+// }
 
-strAndArr(['Сорока', 'Дом', 'Сор', 'Иррациональный', 'Соловей', 'Табурет'], 'со');
+// strAndArr(['Сорока', 'Дом', 'Сор', 'Иррациональный', 'Соловей', 'Табурет'], 'со');
+
+// Задание 3
+num = Math.floor(32.58884);
+a = Math.ceil(32.58884);
+b = Math.round(32.58884);
+
+console.log(num);
+console.log(a);
+console.log(b);
