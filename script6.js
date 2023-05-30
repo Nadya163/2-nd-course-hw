d18f6c4d209f1f6cbc74ecf4fe3ac1d361aec466
Nadya163 <satara19892202@gmail.com>
2023-05-30 23:20:22 +0600

    Задание 2

--- script6.js
+++ script6.js
@@ -1,2 +1,15 @@
 // Задание 1
-console.log('Hello word'.toUpperCase());
+// console.log('Hello word'.toUpperCase());
+
+// Задание 2
+function strAndArr(arr, str) {
+    let newArr = [];
+    arr.forEach((el) => {
+        if (el.toLowerCase().startsWith(str.toLowerCase())) {
+            newArr.push(el);
+        }
+    });
+    console.log(newArr);
+}
+
+strAndArr(['Сорока', 'Дом', 'Сор', 'Иррациональный', 'Соловей', 'Табурет'], 'со');
